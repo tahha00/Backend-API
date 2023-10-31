fec3074857370ff5f2b5dde5eedcd781099aca84
tahha00 <tahha2k9@gmail.com>
2023-10-31 15:00:33 +0000

    added cors middleware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express")
 const cors = require("cors")
 const fs = require("fs")
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 const logger = require("./logger")
 
